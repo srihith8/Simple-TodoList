4265f6d4282e0ed388aa44278834efb3bf9523cb
srihith8 <139107826+srihith8@users.noreply.github.com>
2023-08-17 22:43:22 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Mainpage from './components/Mainpage';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Mainpage />
+    </>
+  );
+}
+
+export default App;
